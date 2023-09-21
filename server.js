5573ed2ae91dd9033db968a5d1c3d99ac0b28c80
horlard <oladipuposaulih@gmail.com>
2023-09-21 09:50:50 +0100

    cors update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
 const connectDb = require("./config/dbConnection");
+const cors = require("cors");
 const dotenv = require("dotenv").config();
 
 const app = express();
@@ -8,6 +9,8 @@ const app = express();
 connectDb();
 
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.use(express.json());
 app.use("/api/transcripts", require("./routes/transcriptRoutes"));
